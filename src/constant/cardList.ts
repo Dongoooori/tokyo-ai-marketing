c81a7e6369913516d14b6f576a8df1d29a606715
シン・ドングン <>
2025-08-16 13:54:27 +0900

    fix_Card

--- src/constant/cardList.ts
+++ src/constant/cardList.ts
@@ -1,9 +1,9 @@
-import Card1 from "../assets/card1.jpg"
-import Card2 from "../assets/card2.jpg"
-import Card3 from "../assets/card3.jpg"
-import Card4 from "../assets/card4.jpg"
-import Card5 from "../assets/card5.jpg"
-import Card6 from "../assets/card6.jpg"
+import Card1 from "../assets/Card1.jpg"
+import Card2 from "../assets/Card2.jpg"
+import Card3 from "../assets/Card3.jpg"
+import Card4 from "../assets/Card4.jpg"
+import Card5 from "../assets/Card5.jpg"
+import Card6 from "../assets/Card6.jpg"
 
 export const cardList = [
   {

865fe4627979df26a2b26942fcfe680d575588e4
シン・ドングン <>
2025-08-17 18:06:05 +0900

    fix/video_poster

--- src/constant/videoList.ts
+++ src/constant/videoList.ts
@@ -1,20 +1,26 @@
 export const videoList = [
   {
-    src: "/video/video1.mp4",//
+    src: "/video/video1.mp4",
+    poster: "/video/video1.jpg",
   },
   {
-    src: "/video/video9.mp4",//
+    src: "/video/video9.mp4",
+    poster: "/video/video9.jpg",
   },
   {
-    src: "/video/video3.mp4",//
+    src: "/video/video3.mp4",
+    poster: "/video/video3.jpg",
   },
   {
     src: "/video/video5.mp4",//
+    poster: "/video/video5.jpg",
   },
   {
-    src: "/video/video4.mp4",
+    src: "/video/video4.mp4",//
+    poster: "/video/video4.jpg",
   },
   {
     src: "/video/video6.mp4",
+    poster: "/video/video6.jpg",
   },
 ]
